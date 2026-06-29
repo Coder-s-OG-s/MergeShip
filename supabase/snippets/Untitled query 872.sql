2cd38c4af112d038fa314f095e63a907381940d1
BiTech5 <bikramkhatri050@gmail.com>
2026-06-29 06:46:44 +0545

    removing local supabase studio query snippet

--- supabase/snippets/Untitled query 872.sql
+++
@@ -1,2 +0,0 @@
-insert into cohorts (slug, name, ends_at) values ('season-1','Season-1','2026-07-25');
-insert into cohort_members (cohort_id, user_id) values (1,'00000000-0000-0000-0000-000000000001');
