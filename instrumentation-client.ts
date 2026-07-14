80985175b50ffcf668e2dd718c26e00bec95297d
Ashutosh Dash <dashashutosh1999@gmail.com>
2026-07-15 00:01:37 +0530

    feat: initialize PostHog only if project token is available

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -1,6 +1,13 @@
 import posthog from 'posthog-js';
 
-posthog.init(process.env.NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN!, {
-  api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-  defaults: '2026-05-30',
-});
+const posthogToken = process.env.NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN;
+
+if (posthogToken) {
+  posthog.init(posthogToken, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    defaults: '2026-05-30',
+    autocapture: false,
+    capture_pageview: false,
+    capture_pageleave: false,
+  });
+}
